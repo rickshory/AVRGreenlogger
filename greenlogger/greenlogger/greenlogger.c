cd4643e5db8f6bf09e040a1f98585619da641131
Rick Shory <rickshory@gmail.com>
2011-12-27 17:36:54 -0800

    First simple prototype. Only sends counting numbers out the serial port.

---
+++ greenlogger/greenlogger/greenlogger.c
@@ -0,0 +1,99 @@
+/**
+ * \file
+ *
+ * \brief code for NDVI (greeness) logger, base on AVR ATmega1284P
+ *
+ * Copyright (C) 2011 Rick Shory, based in part on source code that is:
+ * Copyright (C) 2011 Atmel Corporation. All rights reserved.
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ * Atmel AVR product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+/**
+/*
+ * greenlogger.c
+ *
+ * Created: 12/21/2011 10:04:15 AM
+ *  Author: Rick Shory
+ */ 
+
+#include <avr/io.h>
+#include "greenlogger.h"
+
+
+// the string we send and receive on UART
+const char test_string[] = "Count \n";
+char num_string[20];
+
+/**
+ * \brief The main application
+ *
+ * This application will initialize the UART, send a character and then receive
+ * it and check if it is the same character as was sent.
+ *
+ * \note The RX and TX pins should be externally connected in order to pass the
+ * test.
+ */
+int main(void)
+{
+	uint8_t data;
+	uint8_t cnt;
+	uint16_t cntout = 0;
+	cli();
+	uart_init();
+	sei();
+
+	// Send the test string
+	for (cnt = 0; cnt < strlen(test_string); cnt++) {
+		uart_putchar(test_string[cnt]);
+	}
+    // output a counter
+	do {
+		itoa(cntout, num_string, 10);
+		for (cnt = 0; cnt < strlen(num_string); cnt++) {
+			uart_putchar(num_string[cnt]);
+		}
+		uart_putchar('\n');
+		delay_ms(1000);
+		cntout++;
+		
+	} while (cntout < 65500);
+	// Check if we have received the string we sent
+	cnt = 0;
+	do {
+		// Wait for next character
+		while (!uart_char_waiting());
+		data = uart_getchar();
+		// Compare to what we sent
+//		Assert (data == test_string[cnt++]);
+	} while (cnt < strlen(test_string));
+
+	while (1);
+}
---
+++ greenlogger/greenlogger/greenlogger.h
@@ -0,0 +1,17 @@
+/*
+ * greenlogger.h
+ *
+ * Created: 12/27/2011 1:37:14 PM
+ *  Author: rshory
+ */ 
+
+
+#ifndef GREENLOGGER_H_
+#define GREENLOGGER_H_
+#include "interrupt.h"
+#include "mega_uart_interrupt.h"
+
+
+
+
+#endif /* GREENLOGGER_H_ */

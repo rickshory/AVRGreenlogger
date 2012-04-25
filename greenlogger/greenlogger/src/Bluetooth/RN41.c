16560cacf427d7a233304e8718f160ab0dd72dd6
Rick Shory <rickshory@gmail.com>
2012-04-25 13:32:57 -0700

    Before fixing filenames.

--- greenlogger/greenlogger/src/Bluetooth/RN41.c
+++ greenlogger/greenlogger/src/Bluetooth/RN41.c
@@ -11,53 +11,66 @@
 #include "../greenlogger.h"
 #include <util/twi.h>
 
+/**
+ * \brief turns on power to the RN-42 Bluetooth module
+ *
+ * PortD, bit 4 controls power to the RN-42 Bluetooth module,
+ * high = enabled,
+ */
 inline void BT_power_on(void)
 {
 	// Disable interrupts
 //	cli();
-	// PortD, bit 4 controls power to the RN-41 Bluetooth module
-	// high = enabled
 	DDRD |= (1<<4); // make output
 	PORTD |= (1<<4); // set high; power on
 	// Re-enable interrupts
 //	sei();
 }
 
+/**
+ * \brief turns off power to the RN-42 Bluetooth module
+ *
+ * PortD, bit 4 controls power to the RN-42 Bluetooth module,
+ * high = enabled,
+ */
 inline void BT_power_off(void)
 {
 	// Disable interrupts
 //	cli();
-	// PortD, bit 4 controls power to the RN-41 Bluetooth module
-	// high = enabled
 	DDRD |= (1<<4); // make output
 	PORTD &= ~(1<<4); // set low; turn off
 	// Re-enable interrupts
 //	sei();
 }
 
+/**
+ * \brief sets RN-42 Bluetooth module baud rate to 9600
+ *
+ * PortD, bit 7 controls the BAUD rate of the RN-42 Bluetooth module: 
+ * high = 9600, low = 115k or firmware setting
+ */
 inline void BT_baud_9600(void)
 {
 	// Disable interrupts
 //	cli();
-	// PortD, bit 7 controls the BAUD rate of the RN-41 Bluetooth module
-	// high = 9600
-	// low = 115k or firmware setting
 	DDRD |= (1<<7); // make output
 	PORTD |= (1<<7); // set high, baud 9600
 	// Re-enable interrupts
 //	sei();
 }
 
+/**
+ * \brief sets RN-42 Bluetooth module baud rate to 115k
+ *
+ * PortD, bit 7 controls the BAUD rate of the RN-42 Bluetooth module: 
+ * high = 9600, low = 115k or firmware setting
+ */
 inline void BT_baud_115k(void)
 {
 	// Disable interrupts
 //	cli();
-	// PortD, bit 7 controls the BAUD rate of the RN-41 Bluetooth module
-	// high = 9600
-	// low = 115k or firmware setting
 	DDRD |= (1<<7); // make output
 	PORTD &= ~(1<<7); // set low, baud 115k
 	// Re-enable interrupts
 //	sei();
 }
-

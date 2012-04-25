ddba476af9d12e83d210b1b47ae420eb4d73fb81
Rick Shory <rickshory@gmail.com>
2012-04-25 11:03:01 -0700

    Init Bluetooth file setup.

---
+++ greenlogger/greenlogger/src/Bluetooth/RN41.c
@@ -0,0 +1,12 @@
+/*
+ * RN41.c
+ *
+ * Created: 4/25/2012 10:50:13 AM
+ *  Author: rshory
+ */ 
+
+#include <inttypes.h>
+#include "RN41.h"
+#include "../I2C/I2C.h"
+#include "../greenlogger.h"
+#include <util/twi.h>
---
+++ greenlogger/greenlogger/src/Bluetooth/RN41.h
@@ -0,0 +1,16 @@
+/*
+ * RN41.h
+ *
+ * Created: 4/25/2012 10:49:34 AM
+ *  Author: rshory
+ */ 
+
+
+#ifndef RN41_H_
+#define RN41_H_
+
+
+
+
+
+#endif /* RN41_H_ */

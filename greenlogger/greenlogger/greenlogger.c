7b283955e7c2da1e3939f7bda1978fe5ce39a0a2
Rick Shory <rickshory@gmail.com>
2013-02-13 09:57:16 -0800

    try longer blink time

--- greenlogger/greenlogger/greenlogger.c
+++ greenlogger/greenlogger/greenlogger.c
@@ -365,7 +365,7 @@ int main(void)
 			// if not time to log data, and not roused
 			if ((!(timeFlags & (1<<timeToLogData))) && (!((stateFlags1 & (1<<isRoused))))) {
 				// won't do anything with results anyway, don't bother reading sensors, save power
-				stayRoused(20); // rouse for 0.2 second to flash the pilot light
+				stayRoused(50); // rouse for 0.5 second to flash the pilot light
 				break; 
 			}
 

eead023b3ce07dddcecd557b6ecebdf890521b9d
Rick Shory <rickshory@gmail.com>
2013-07-23 11:26:04 -0700

    GPS files created & tracked, still empty

---
+++ greenlogger/greenlogger/src/GPS/A2035-H.c
@@ -0,0 +1,6 @@
+/*
+ * A2035_H.c
+ *
+ * Created: 7/23/2013 11:12:49 AM
+ *  Author: rshory
+ */ 
---
+++ greenlogger/greenlogger/src/GPS/A2035-H.h
@@ -0,0 +1,16 @@
+/*
+ * A2035_H.h
+ *
+ * Created: 7/23/2013 11:13:14 AM
+ *  Author: rshory
+ */ 
+
+
+#ifndef A2035-H_H_
+#define A2035-H_H_
+
+
+
+
+
+#endif /* A2035-H_H_ */

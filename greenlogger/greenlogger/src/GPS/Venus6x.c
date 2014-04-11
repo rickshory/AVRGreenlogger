28bd1819e3ddd4cb20792563f84b88d1854b52eb
Rick Shory <rickshory@gmail.com>
2014-04-10 21:10:00 -0700

    GPS Venus6x.c and Venus6x.h files created and tracked, still empty

---
+++ greenlogger/greenlogger/src/GPS/Venus6x.c
@@ -0,0 +1,6 @@
+/*
+ * CProgram1.c
+ *
+ * Created: 4/10/2014 8:59:10 PM
+ *  Author: rshory
+ */ 
---
+++ greenlogger/greenlogger/src/GPS/Venus6x.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 4/10/2014 9:01:25 PM
+ *  Author: rshory
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+
+
+
+
+#endif /* INCFILE1_H_ */

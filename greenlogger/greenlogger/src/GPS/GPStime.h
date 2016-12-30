13d3a45c57c581df904c810202a3d7c3aeaae437
Rick Shory <rickshory@gmail.com>
2016-12-30 12:02:34 -0800

    changed struct name from chargePoint to chargeInfo

--- greenlogger/greenlogger/src/GPS/GPStime.h
+++ greenlogger/greenlogger/src/GPS/GPStime.h
@@ -11,15 +11,15 @@
 
 #include "compiler.h"
 
-#define DAYS_FOR_MOVING_AVERAGE 16 // sets size of the array of chargePoints
+#define DAYS_FOR_MOVING_AVERAGE 16 // sets size of the array of chargeInfo's
 
 typedef volatile struct { // used for tracking the cell voltage daily maximum, when
 						// most power is available for tasks whose timing is 
 						// flexible, such as reading the GPS
-	uint16_t chargeLevel; // ADC reading of the cell voltage
+	uint16_t level; // ADC reading of the cell voltage
 	uint16_t minuteOfDay; // which minute of the day (max 1440) this charge level was read
 	// may regret not making this a dateTime
-} chargePoint ;
+} chargeInfo ;
 
 extern inline bool GPS_powered(void);
 extern inline void GPS_power_on(void);
